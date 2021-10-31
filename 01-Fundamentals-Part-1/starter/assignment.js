160a9e29e69465009ee29bcfb951ace99e3f6f6c
Sean <Sransonette@gmail.com>
2021-10-30 19:44:14 -0500

    test to make sure my answers show up

---
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -0,0 +1,13 @@
+let country = "USA";
+let continent = "North America";
+let population = "Lots";
+
+// console.log(country, continent, population)
+
+let isIsland = false
+let language
+
+console.log(typeof isIsland)
+console.log(typeof population)
+console.log(typeof country)
+console.log(typeof language)
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,45 @@
+// let js = "Amazing";
+// console.log(40 + 8 + 23 - 10)
+
+// console.log("Jonas");
+// console.log(23);
+
+// let firstName = "Matilda";
+
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+
+// let jonas_matilda = "JM";
+// let $function = 27;
+
+// let person = "Jonas"
+// let PI = 3.1415;
+
+// let myfirstJob = "Coder";
+// let myCurrentJob = "Teacher";
+
+
+// console.log(myfirstJob);
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 32);
+// console.log(typeof "Jonas");
+
+javascriptIsFun = "YES";
+console.log(javascriptIsFun)
+console.log(typeof javascriptIsFun);
+
+let year;
+
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null)
