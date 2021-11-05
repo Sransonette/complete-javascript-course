5c9141540c9e3dbf25197a66dbb0fd7d0b976dde
Sean <Sransonette@gmail.com>
2021-11-04 21:33:43 -0500

    operator precedence answer

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,96 +1,105 @@
-// let js = "Amazing";
-// console.log(40 + 8 + 23 - 10)
+// // let js = "Amazing";
+// // console.log(40 + 8 + 23 - 10)
 
-// console.log("Jonas");
-// console.log(23);
+// // console.log("Jonas");
+// // console.log(23);
 
-// let firstName = "Matilda";
+// // let firstName = "Matilda";
 
-// console.log(firstName);
-// console.log(firstName);
-// console.log(firstName);
+// // console.log(firstName);
+// // console.log(firstName);
+// // console.log(firstName);
 
-// let jonas_matilda = "JM";
-// let $function = 27;
+// // let jonas_matilda = "JM";
+// // let $function = 27;
 
-// let person = "Jonas"
-// let PI = 3.1415;
+// // let person = "Jonas"
+// // let PI = 3.1415;
 
-// let myfirstJob = "Coder";
-// let myCurrentJob = "Teacher";
+// // let myfirstJob = "Coder";
+// // let myCurrentJob = "Teacher";
 
 
-// console.log(myfirstJob);
+// // console.log(myfirstJob);
 
-// let javascriptIsFun = true;
-// console.log(javascriptIsFun);
+// // let javascriptIsFun = true;
+// // console.log(javascriptIsFun);
 
-// // console.log(typeof true);
-// console.log(typeof javascriptIsFun);
-// // console.log(typeof 32);
-// // console.log(typeof "Jonas");
+// // // console.log(typeof true);
+// // console.log(typeof javascriptIsFun);
+// // // console.log(typeof 32);
+// // // console.log(typeof "Jonas");
 
-// javascriptIsFun = "YES";
-// console.log(javascriptIsFun)
-// console.log(typeof javascriptIsFun);
+// // javascriptIsFun = "YES";
+// // console.log(javascriptIsFun)
+// // console.log(typeof javascriptIsFun);
 
-// let year;
+// // let year;
 
-// console.log(year);
-// console.log(typeof year);
+// // console.log(year);
+// // console.log(typeof year);
 
-// year = 1991;
-// console.log(typeof year);
+// // year = 1991;
+// // console.log(typeof year);
 
-// console.log(typeof null)
+// // console.log(typeof null)
 
-// let age = 30;
-// age = 31;
-// //Mutated the variable, block scope
-// const birthYear = 1991;
-// birthYear = 1990;
-// //in-mutable and cannot set empty variable in const
-// var job = 'programmer';
-// job = 'teacher'
-// //should be avoided for the most part, function scoped
-// lastName = "Ransonette"
-// console.log(lastName);
-// //creates property on "global object", always declare variables
+// // let age = 30;
+// // age = 31;
+// // //Mutated the variable, block scope
+// // const birthYear = 1991;
+// // birthYear = 1990;
+// // //in-mutable and cannot set empty variable in const
+// // var job = 'programmer';
+// // job = 'teacher'
+// // //should be avoided for the most part, function scoped
+// // lastName = "Ransonette"
+// // console.log(lastName);
+// // //creates property on "global object", always declare variables
+
+// const now = 2037
+// const ageJonas = now - 1991
+// const ageSarah = now - 2018
+// console.log(ageJonas, ageSarah)
+
+// console.log(ageJonas * 2, ageJonas / 10, 2 ** 3)
+// // 2 ** 3 means 2 to the power of 3
+
+// const firstName = 'Jonas'
+// const lastName = 'Ransonette'
+// console.log(firstName + ' ' + lastName)
+
+// let x = 10 + 5;
+// console.log(x)
+
+// x += 10; // x = x + 10
+// x *= 4;
+// x++;
+// x--;
+// console.log(x)
+
+// //Comparison operators
+
+// console.log(ageJonas > ageSarah);
+// console.log(ageSarah >= 18)
+
+// const isFullAge = ageSarah >= 18;
+
+
+
 
 const now = 2037
 const ageJonas = now - 1991
 const ageSarah = now - 2018
-console.log(ageJonas, ageSarah)
-
-console.log(ageJonas * 2, ageJonas / 10, 2 ** 3)
-// 2 ** 3 means 2 to the power of 3
-
-const firstName = 'Jonas'
-const lastName = 'Ransonette'
-console.log(firstName + ' ' + lastName)
-
-let x = 10 + 5;
-console.log(x)
-
-x += 10; // x = x + 10
-x *= 4;
-x++;
-x--;
-console.log(x)
-
-//Comparison operators
-
-console.log(ageJonas > ageSarah);
-console.log(ageSarah >= 18)
-
-const isFullAge = ageSarah >= 18;
 
 console.log(now - 1991 > now - 2018);
 
+let x, y;
 
+x = y = 25 - 10 - 5
 
+console.log(x, y)
 
-
-
-
+const averageAge = (ageJonas + ageSarah) / 2
+console.log(ageJonas, ageSarah, averageAge)
 
