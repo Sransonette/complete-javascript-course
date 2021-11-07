f08bd90a523c9ac1f2faa78c6f3d8dfa1b6d2cd4
Sean <Sransonette@gmail.com>
2021-11-06 22:54:30 -0500

    fixed errors in index.html

--- 01-Fundamentals-Part-1/starter/assignment.js
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -12,4 +12,14 @@
 // console.log(typeof country)
 // console.log(typeof language)
 
-test
+let markW = 78
+let markH = 1.69
+let johnW = 92
+let johnH = 1.95
+
+let markBMI = markW / markH ** 2;
+let johnBMI = johnW / johnH ** 2;
+let markHIgherBMI = markBMI > johnBMI
+
+console.log(markW)
+
