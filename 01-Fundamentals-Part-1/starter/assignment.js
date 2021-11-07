3026723b6e8d5eac2e8001b82b31ba73c2b01598
Sean <Sransonette@gmail.com>
2021-11-06 22:59:30 -0500

    challenge 1 answers part 2

--- 01-Fundamentals-Part-1/starter/assignment.js
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -12,13 +12,18 @@
 // console.log(typeof country)
 // console.log(typeof language)
 
-let markW = 78
-let markH = 1.69
-let johnW = 92
-let johnH = 1.95
+// let markWeight = 78
+// let markHeight = 1.69
+// let johnWeight = 92
+// let johnHeight = 1.95
 
-let markBMI = markW / markH ** 2;
-let johnBMI = johnW / johnH ** 2;
+let markWeight = 95
+let markHeight = 1.88
+let johnWeight = 85
+let johnHeight = 1.76
+
+let markBMI = markWeight / markHeight ** 2;
+let johnBMI = johnWeight / johnHeight ** 2;
 let markHIgherBMI = markBMI > johnBMI
 
 console.log(markBMI, johnBMI, markHIgherBMI);
