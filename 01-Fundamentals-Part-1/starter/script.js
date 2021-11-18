956fe5490961903b8cf67532880341f13b5b44fb
Sean <Sransonette@gmail.com>
2021-11-17 21:25:06 -0600

    section 2.21 answers. Let push should have said 2.20 my bad.

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -146,15 +146,36 @@
 // console.log(century)
 
 //type conversion
-let inputYear = "1991";
-console.log(Number(inputYear))
-console.log(Number(inputYear) + 18)
-console.log(Number("Sean"))
-console.log(typeof NaN)
-console.log(String(23), 23)
+// let inputYear = "1991";
+// console.log(Number(inputYear))
+// console.log(Number(inputYear) + 18)
+// console.log(Number("Sean"))
+// console.log(typeof NaN)
+// console.log(String(23), 23)
 
-//type coercion
-console.log('I am' + 23 + 'years old')
-console.log('23' - '10' - 3)
-console.log('23' / '2')
+// //type coercion
+// console.log('I am' + 23 + 'years old')
+// console.log('23' - '10' - 3)
+// console.log('23' / '2')
 
+// 5 falsy values: 0, '', undefined, null, NaN
+
+console.log(Boolean(0))
+console.log(Boolean(undefined))
+console.log(Boolean('Sean'))
+console.log(Boolean(''))
+
+let money = 100;
+
+if (money) {
+    console.log("Don't spend it all")
+} else {
+    console.log('You should get a job')
+}
+
+let height = 0;
+if (height >= 0) {
+    console.log('YAY! Height is defined')
+} else {
+    console.log('Height is UNDEFINED')
+}
