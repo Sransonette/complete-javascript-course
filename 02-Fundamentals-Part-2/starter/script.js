c2511886b673f21c4734c5aaddcc8fab16d7df55
Sean <Sransonette@gmail.com>
2021-11-24 00:19:28 -0600

    section 2.33 answers

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,11 +1,31 @@
-'use strict';
+// 'use strict';
 
-let hasDriversLicense = false;
-const passTest = true;
+// let hasDriversLicense = false;
+// const passTest = true;
 
-if (passTest) hasDriversLicense = true
-if (hasDriversLicense) console.log("I can drive")
+// if (passTest) hasDriversLicense = true
+// if (hasDriversLicense) console.log("I can drive")
 
 // const interface = 'Audio'
 // const private = 534;
 // const if = 23
+
+function logger() {
+    console.log('My name is Sean')
+}
+
+// calling / running / invoking the function
+logger()
+logger()
+logger()
+
+function fruitProcessor(apples, oranges) {
+    let juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice
+}
+
+let appleJuice = fruitProcessor(5, 0)
+console.log(appleJuice)
+
+let appleOrangeJuice = fruitProcessor(2, 4)
+console.log(appleOrangeJuice)
